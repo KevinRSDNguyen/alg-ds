93eb64ac70d3a4f3a90be8fa64b06471a86e22d0
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-08-30 17:32:55 -0700

    Updated Hint for twoSum Alg

--- exercises/twoSum/index.js
+++ exercises/twoSum/index.js
@@ -1,8 +1,8 @@
 // --- Directions
 // Given an array of numbers, return all pairs that add up to a given sum.
-// The numbers can be used more than once.
+// The numbers can be used more than once. Use a hash map!
 // --- Examples:
-// deepEqual(twoSum([1, 2, 2, 3, 4], 4) --> [[2, 2], [3, 1]]
+// twoSum([1, 2, 2, 3, 4], 4) --> [[2, 2], [3, 1]]
 
 function twoSum(array, sum) {}
 
