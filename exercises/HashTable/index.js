19dc9535934798f30b915d29503cfd4bdd9a265f
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-09-13 20:03:09 -0700

    Added HashTable DS

---
+++ exercises/HashTable/index.js
@@ -0,0 +1,31 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+  set(key, value) {}
+  get(key) {}
+  keys() {}
+  values() {}
+}
+
+let ht = new HashTable(17);
+ht.set("maroon", "#800000");
+ht.set("yellow", "#FFFF00");
+ht.set("olive", "#808000");
+ht.set("salmon", "#FA8072");
+ht.set("lightcoral", "#F08080");
+ht.set("mediumvioletred", "#C71585");
+ht.set("plum", "#DDA0DD");
+
+module.exports = { HashTable };
---
+++ exercises/HashTable/test.js
@@ -0,0 +1,45 @@
+const { HashTable } = require("./index");
+let ht = new HashTable(17);
+ht.set("maroon", "#800000");
+ht.set("yellow", "#FFFF00");
+ht.set("olive", "#808000");
+ht.set("salmon", "#FA8072");
+ht.set("lightcoral", "#F08080");
+ht.set("mediumvioletred", "#C71585");
+ht.set("plum", "#DDA0DD");
+ht.set("dupe", "#DDA0DD");
+
+test("Set works and uses seperate-chaining", () => {
+  expect(ht.keyMap[0]).toBeTruthy();
+  expect(ht.keyMap[2]).toBeFalsy();
+  expect(ht.keyMap[8].length).toEqual(2);
+});
+
+test("get works and returns the value of key", () => {
+  expect(ht.get("yellow")).toEqual("#FFFF00");
+});
+
+test("keys() returns an array of all key values", () => {
+  expect(ht.keys()).toEqual([
+    "plum",
+    "salmon",
+    "dupe",
+    "maroon",
+    "yellow",
+    "olive",
+    "lightcoral",
+    "mediumvioletred"
+  ]);
+});
+
+test("values() returns an array of values with no dupes!", () => {
+  expect(ht.values()).toEqual([
+    "#DDA0DD",
+    "#FA8072",
+    "#800000",
+    "#FFFF00",
+    "#808000",
+    "#F08080",
+    "#C71585"
+  ]);
+});
